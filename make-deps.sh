e6ac6411554e45b0b1f05f644de86430447b67d8
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2018-10-31 15:28:45 -0700

    Fix dependency updating script.
    The script now generates the following:
    * sources.txt   The sources section for the .spec file, including patch
                    section for textual repository metadata.
    * install.txt   The install section commands.
    * files.txt     The complete files section.
    * patches       The patches for maven-metadata.xml in all the locations
                    where it exists in the maven repository.
    
    Also, update .gitignore.

--- make-deps.sh
+++ make-deps.sh
@@ -49,14 +49,32 @@ tar xzf "${HADOOP_TGZ}"
 ### fetch the hadoop depenencies and store the log (to retrieve the urls)
 
 cd "${HADOOP_DIR}"
-mvn dependency:resolve | grep "^Downloading:" | sed -e 's/^Downloading: \(\S\+\)\s*$/\1/' > "${REPO_DIR}/urls.txt"
+
+# patch it as per the hadoop .spec (assume files do not contain spaces)
+PATCHES=$(grep ^Patch "${REPO_DIR}/../apache-hadoop/apache-hadoop.spec" | sed -e 's/Patch[0-9]\+\s*:\s*\(\S\)\s*/\1/')
+
+for p in $PATCHES; do
+    patch -p1 < "${REPO_DIR}/../apache-hadoop/${p}"
+done
+
+mvn package -Pnative -Pdist -DskipTests -Dtar || exit 1
 
 cd "${REPO_DIR}"
 
+# remove previously created artifacts
+rm -f sources.txt install.txt files.txt metadata-*.patch
+
 ### make the list of the dependencies
-DEPENDENCIES=($(find ${HOME}/.m2/repository -type f -name *.jar -o -name *.pom))
+DEPENDENCIES=($(find ${HOME}/.m2/repository -type f -name \*.jar -o -name \*.pom))
+METADATA=($(find ${HOME}/.m2/repository -type f -name maven-metadata\*.xml))
 
 ### create pieces of the spec (SourceXXX definitions and their install actions)
+
+# some of the maven repositories do not allow direct download, so use single
+# repository instead: https://repo1.maven.org/maven2/ . It the same as
+# https://central.maven.org, but central.maven.org uses bad certificate (FQDN
+# mismatch).
+REPOSITORY_URL=https://repo1.maven.org/maven2/
 
 SOURCES_SECTION=""
 INSTALL_SECTION=""
@@ -64,49 +82,54 @@ FILES_SECTION=""
 warn=
 n=0
 
-for dep in "${DEPENDENCIES[@]}"; do
+for dep in ${DEPENDENCIES[@]}; do
     dep=${dep##${HOME}/.m2/repository/}
     dep_bn=$(basename "$dep")
     dep_dn=$(dirname "$dep")
-    dep_dn=${dep_dn##"${HOME}/.m2/repository/"}
-    dep_url=$(grep "${dep_bn}" urls.txt | tail -1 | sed -e 's/^\s*//' -e 's/\s*$//')
-    if [ -z "${dep_url}" ]; then
-        # try to see if we had it before
-        dep_url=$(grep "Source[0-9]\+\s*:.*${dep_bn}" hadoop-dep.spec | sed -e 's/^Source[0-9]\+\s*:\s*\(\S\+\)\s*$/\1/')
-        if [ -z "${dep_url}" ]; then
-            2>&1 echo Cannot find url for file: $dep_bn:
-            2>&1 grep "${dep_bn}" urls.txt
-            dep_url="FIXME: ${dep_bn}"
-            warn=1
-        fi
-    fi
+    dep_url=${REPOSITORY_URL}${dep}
     SOURCES_SECTION="${SOURCES_SECTION}
 Source${n} : ${dep_url}"
     INSTALL_SECTION="${INSTALL_SECTION}
 mkdir -p %{buildroot}/usr/share/apache-hadoop/.m2/repository/${dep_dn}
 cp %{SOURCE${n}} %{buildroot}/usr/share/apache-hadoop/.m2/repository/${dep_dn}"
     FILES_SECTION="${FILES_SECTION}
-/usr/share/apache-hadoop/.m2/repository/${dep_dn}"
-    let n=$n+1
+/usr/share/apache-hadoop/.m2/repository/${dep}"
+    let n=${n}+1
 done
+
+# for each of the maven-metadata, generate a patch
+if [ -n "${METADATA}" ]; then
+    cd ${HOME}/.m2/repository
+    n=0
+    for md in ${METADATA[@]}; do
+        md=${md##${HOME}/.m2/repository/}
+        diff -u /dev/null "${md}" > ${REPO_DIR}/metadata-${n}.patch
+        SOURCES_SECTION="${SOURCES_SECTION}
+Patch${n} : metadata-${n}.patch"
+        FILES_SECTION="${FILES_SECTION}
+/usr/share/apache-hadoop/.m2/repository/${md}"
+        let n=${n}+1
+    done
+fi
+
+cd "${REPO_DIR}"
 
 echo "${SOURCES_SECTION}" | sed -e '1d' > sources.txt
 echo "${INSTALL_SECTION}" | sed -e '1d' > install.txt
 echo "${FILES_SECTION}" | sed -e '1d' > files.txt
 
-if [ "${warn}" -eq 1 ]; then
-    cat <<EOF
-WARNING: Some URs are missing. sources.txt is not usable as is. Fix the
-WARNING following issues in sources.txt:
-EOF
-    grep 'FIXME:' sources.txt
-fi
 cat <<EOF
 
-sources.txt contains SourceXXXX definitions for the spec file.
-install.txt contains %install section.
-files.txt contains the %files section.
+sources.txt     contains SourceXXXX definitions for the spec file (including
+                patches for metadata).
+install.txt     contains %install section.
+files.txt       contains the %files section.
 EOF
+
+if [ -n "${METADATA}" ]; then
+    echo Metadata patches:
+    ls -1 metadata-*.patch
+fi
 
 # restore previous .m2
 rm -rf ${HOME}/.m2/repository
